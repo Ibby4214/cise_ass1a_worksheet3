5a0747617b2d0332ba0b9402d08eb6ee91bcce7a
ibby4214 <you@example.com>
2021-08-08 22:06:23 +1200

    third

--- app.js
+++ app.js
@@ -1,28 +1,46 @@
-// app.js
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
-const express = require('express');
-const connectDB = require('./config/db');
-var cors = require('cors');
-
-// routes
-const books = require('./routes/api/books');
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
-// Connect Database
-connectDB();
+//import your models
+require("./models/quote");
 
-// cors
-app.use(cors({ origin: true, credentials: true }));
+mongoose
+  .connect(
+    process.env.MONGODB_CONNECTION_STRING,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("MongoDB has been connected"))
+  .catch((err) => console.log(err));
 
-// Init Middleware
-app.use(express.json({ extended: false }));
+//middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Hello world!'));
+//import routes
+require("./routes/quoteRoute.js")(app);
 
-// use Routes
-app.use('/api/books', books);
+const PORT = process.env.PORT || 5000;
 
-const port = process.env.PORT || 8082;
+// Accessing the path module
+const path = require("path");
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
