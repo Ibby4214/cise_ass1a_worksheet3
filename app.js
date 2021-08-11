9d70f47052cb68825e3a2f1c5308bae1e69d07d2
ibby4214 <you@example.com>
2021-08-11 21:03:23 +1200

    commit1 message

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
+// // app.js
+
+
+// const express = require('express');
+// const connectDB = require('./config/db');
+// var cors = require('cors');
+
+
+// // routes
+// const books = require('./routes/api/books');
+
+// const app = express();
+
+// // Connect Database
+// connectDB();
+
+// // cors
+// app.use(cors({ origin: true, credentials: true }));
+
+// // Init Middleware
+// app.use(express.json({ extended: false }));
+
+// app.get('/', (req, res) => res.send('Hello world!'));
+
+// // use Routes
+// app.use('/api/books', books);
+
+// const port = process.env.PORT || 8082;
+
+// app.listen(port, () => console.log(`Server running on port ${port}`));
+
+
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
@@ -44,3 +76,4 @@ app.get("*", function (request, response) {
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
+
